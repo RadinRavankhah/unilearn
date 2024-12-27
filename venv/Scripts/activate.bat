bbf8c77fcc25a66942442b2bb43a7c8ddd7b6c21
MrRad1st <Radin1383rvn@gmail.com>
2024-12-27 22:34:44 +0330

    Communities button added to home page

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\github project\unilearn\venv
+set VIRTUAL_ENV=D:\Github Repos\unilearn\unilearn\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
